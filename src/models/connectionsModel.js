77cb2a7135d809587154b0765cd3d133f6e78823
Nicolás <nicolas.m.giudice@gmail.com>
2023-05-23 23:38:27 -0300

    fix reference table

--- src/models/connectionsModel.js
+++ src/models/connectionsModel.js
@@ -4,15 +4,19 @@ const Sequelize = require("sequelize");
 /* import models. */
 const productsModel = require("./productsModel");
 const usersModel = require("./usersModel");
+const salesModel = require("./salesModel");
 
 /* models connections. */
 const Products = productsModel(sequelize, Sequelize);
 const Users = usersModel(sequelize, Sequelize);
+
+const Sales = salesModel(sequelize, Sequelize, Users);
 
 /* assosiations.  */
 
 /* models connections exports. */
 module.exports = {
 	Products,
+	Sales,
 	Users,
 };
--- src/models/salesModel.js
+++ src/models/salesModel.js
@@ -1,15 +1,9 @@
 const { TABLES } = require("../const/tableNames");
 
-const { Users, Products } = require("./connectionsModel");
-
-module.exports = (sequelize, type) => {
+module.exports = (sequelize, type, refModel) => {
 	return sequelize.define(TABLES.SALES, {
 		createdAt: {
 			field: "created_at",
-			type: type.DATE,
-		},
-		date: {
-			allowNull: false,
 			type: type.DATE,
 		},
 		deletedAt: {
@@ -25,32 +19,19 @@ module.exports = (sequelize, type) => {
 			allowNull: false,
 			type: type.DOUBLE,
 		},
-		product_id: {
-			allowNull: false,
-			references: {
-				model: Products,
-				key: "id",
-			},
-			type: type.INTEGER,
-		},
-		quantity: {
-			allowNull: false,
-			type: type.INTEGER,
-		},
 		updatedAt: {
 			field: "updated_at",
 			type: type.DATE,
 		},
 		user_id: {
-			allowNull: false,
 			references: {
-				model: Users,
+				model: refModel,
 				key: "id",
 			},
 			type: type.INTEGER,
 		},
 	},
 	{
-		paranoid: true
+		paranoid: true,
 	});
 };
