84098266705594a7cf3d6ef2984eafe659bb4508
ManuelCastanon <manucasta01@gmail.com>
2023-05-25 22:19:05 -0300

    saleControllerDelete

--- src/routes/sale/saleRoutes.js
+++ src/routes/sale/saleRoutes.js
@@ -16,7 +16,7 @@ class SaleRoutes {
 		this.router.get("/", authLoggedInUser(), this.controller.getPage);
 		this.router.get("/:id", authLoggedInUser(), this.controller.getOne);
 		//this.router.put("/:id", authLoggedInUser(), this.controller.update);
-		//this.router.delete("/:id", authLoggedInUser(), this.controller.delete);
+		this.router.delete("/:id", authLoggedInUser(), this.controller.delete);
 		return this.router;
 	}
 }
